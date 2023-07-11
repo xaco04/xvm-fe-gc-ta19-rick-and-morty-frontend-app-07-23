b508d352a122a113e98247010c9e41d54fa93d98
xaco04 <xavivico04@gmail.com>
2023-07-11 19:16:37 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rickymortyapp_v0.0.3';
+}
